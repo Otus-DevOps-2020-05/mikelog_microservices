287686ae60b9509a93e0249c6c892c5911ab2599
Mikhail Loginov <lmi@t-sk.ru>
2020-08-21 13:32:36 +0300

    docker-2

--- docker-monolith/docker.sh
+++ docker-monolith/docker.sh
@@ -3,34 +3,42 @@ packer_dir=./infra/packer
 ansible_dir=./infra/ansible
 terraform_dir=./infra/terraform
 cd $packer_dir
-if [ $(packer validate -var-file=./variables.json docker-host.json) -eq "" ]
+if [ -z "$(packer validate -var-file=./variables.json docker-host.json)" ]
 then
+  echo "Validation successfull!"
   echo "Truing to build custom image. Whait..."
   build_image=$(packer build -var-file=./variables.json docker-host.json)
+  image_id=0
   image_id=$(echo $build_image| grep "yandex: A disk image was created:" | awk -F"id: " '{print $2}' | awk -F ")" '{print $1}')
-  if [ $image_id -eq ""]
+  if [ -z "$image_id" ]
   then
     echo "Something wrongs"
-    echo $build_image
+    echo "$build_image"
+    exit
   else
    cd ../../$terraform_dir
    t_init=$(terraform init)
-   if [$(echo $t_init | grep 'Terraform has been successfully initialized!' -c) -eq "1"]
+   if [ "$(echo $t_init | grep 'Terraform has been successfully initialized!' -c)" -eq "1" ]
    then
-    sed -E -i 's/image_id(.+)=(.+)"(.+)"/image_id="'$image_id'"/g' terraform.tfvars
-    t_plan=$(terraform plan)
-    if [ $(echo $tplan | grep 'Error:' -c ) -eq "0"]
+    if [ "$(uname -a | grep -i "Darwin\|MacBook" -c)" -gt "0"  ]
+    then
+     sed -E -i '.bak' 's/image_id(.+)=(.+)"(.+)"/image_id="'$image_id'"/g' terraform.tfvars
+    else
+     sed -E -i 's/image_id(.+)=(.+)"(.+)"/image_id="'$image_id'"/g' terraform.tfvars
+    fi
+    t_plan=$(terraform plan 2>&1)
+    if [ "$(echo $t_plan | grep 'Error:' -c)" -eq "0" ]
     then
      echo "Trying to apply terraform. Please whait..."
-     t_apply=$(terraform apply --auto-approve)
-     if [ $(echo $t_apply | grep 'Error:' -c) -eq "0"]
+     t_apply=$(terraform apply --auto-approve 2>&1)
+     if [ "$(echo $t_apply | grep 'Apply complete! Resources:' -c)" -eq "1" ]
      then
       echo $t_apply | awk '/Apply complete! Resources:.*$/ { matched = 1 } matched'
       echo ""
       echo "Trying to provision app in docker"
       cd ../../$ansible_dir
       ansible=$(ansible-playbook  playbooks/provision.yml)
-      if [$(echo $ansible | grep "failed=[1-9]" -c) -eq "0"]
+      if [ "$(echo $ansible | grep "failed=[1-9]" -c)" -eq "0" ]
       then
        echo "Go to http://lb_ip_addr:9292"
       else
